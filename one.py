7d0f8e083dad8db21ad84e4c1a4e13384f089971
Alisa <87872370+sheinkmana@users.noreply.github.com>
2022-12-01 20:28:41 +0000

    Add files via upload

---
+++ one.py
@@ -0,0 +1,24 @@
+with open('input.txt') as f:
+    lines =f.readlines()
+sum_cal = 0
+max_cal = 0
+for l in lines:
+    try:
+        sum_cal += int(l)
+    except:
+        max_cal = max(max_cal, sum_cal)
+        sum_cal = 0
+max_three = [0,0,0]
+sum_caltwo = 0
+for l in lines:
+    try:
+        sum_caltwo += int(l)
+    except:
+        if min(max_three) < sum_caltwo:
+            max_three.remove(min(max_three))
+            max_three.append(sum_caltwo)
+        sum_caltwo = 0
+print(max_cal, max_three, sum(max_three))
+#%%
+
+#%%
