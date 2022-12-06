7eb8fbddfed5e061b8c3cff80a6365a6566b86b8
Alisa <87872370+sheinkmana@users.noreply.github.com>
2022-12-06 18:58:10 +0000

    Update six.py

--- solutions/six.py
+++ solutions/six.py
@@ -3,11 +3,11 @@ with open('input6.txt') as f:
 condition_one = False
 condition_two= False
 for i in range(len(L)):
-    if (not condition_one) and i-3>=0 and \
+    if (not condition_one) and i>=3 and \
             len(set([L[i-j] for j in range(4)]))==4:
         print(i+1)
         condition_one = True
-    if (not condition_two) and i-13>=0 and\
+    if (not condition_two) and i>=13 and\
             len(set([L[i-j] for j in range(14)]))==14:
         print(i+1)
         condition_two = True
