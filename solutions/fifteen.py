380d534064f4f46e38c2867335ddc9fbe894a7c6
Alisa <87872370+sheinkmana@users.noreply.github.com>
2022-12-16 19:48:24 +0000

    Update fifteen.py

--- solutions/fifteen.py
+++ solutions/fifteen.py
@@ -1,7 +1,17 @@
+# My ideas incorporated with two or three works I saw on github 
+# (my ideas only didn't work:(((( )
+
 with open('input15.txt') as file:
     lines = [l for l in file.read().strip().split('\n')]
 
-S = set()
+def closest(x,y,S):
+    for (sensor_x, sensor_y, dist) in S:
+        dist_new = abs(x-sensor_x) + abs(y-sensor_y)
+        if dist_new <= dist:
+            return False
+    return True  
+
+S_d = set()
 B = set()
 
 for line in lines:
@@ -13,20 +23,15 @@ for line in lines:
     beacon_x = int(beacon_x[2:-1])
     beacon_y = int(beacon_y[2:])
     dist = abs(sensor_x - beacon_x) + abs(sensor_y - beacon_y)
-    S.add((sensor_x, sensor_y, dist))
+    S_d.add((sensor_x, sensor_y, dist))
     B.add((beacon_x, beacon_y))
 
-def closest(x,y,S):
-    for (sensor_x, sensor_y, dist) in S:
-        dist_new = abs(x-sensor_x) + abs(y-sensor_y)
-        if dist_new <= dist:
-            return False
-    return True
+
 
 cant = 0
 for x in range(-10000000, 10000000):
     y = 2000000
-    if not closest(x,y,S):
+    if not closest(x,y,S_d):
         if (x,y) not in B:
             cant += 1
 print(cant)
@@ -34,7 +39,7 @@ print(cant)
 
 distress = False
 
-for (sensor_x, sensor_y, dist) in S:
+for (sensor_x, sensor_y, dist) in S_d:
     for x in range(dist+2):
         y = (dist+1) - x
         for tune_x,tune_y in [(-1,-1),(-1,1),(1,-1),(1,1)]:
@@ -43,7 +48,7 @@ for (sensor_x, sensor_y, dist) in S:
             if not (0 <= new_x <= 4000000) or not (0 <= new_y <=4000000):
                 continue
             assert abs(new_x-sensor_x)+abs(new_y-sensor_y) == dist + 1
-            if closest(new_x,new_y,S):
+            if closest(new_x,new_y,S_d):
                 if (not distress):
                     print(new_x*4000000 + new_y)
                     distress = True
